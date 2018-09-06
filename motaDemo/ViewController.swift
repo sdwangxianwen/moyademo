14108d7b165311900c05d67e3a60f82303893637
wangwenwen <wangwenwen@51Talk.com>
2018-09-06 15:00:09 +0800

    代码优化

--- motaDemo/ViewController.swift
+++ motaDemo/ViewController.swift
@@ -34,13 +34,11 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     var tableview :UITableView! //tableview
     var arrM : NSMutableArray!  //存放数据源的可变数组
     var isAfn : Bool!
-    
-    
+  
     //MARK:tableview的代理方法和数据源方法
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.arrM.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let identify:String = "SwiftCell"
         let cell = tableView.dequeueReusableCell(
@@ -49,29 +47,32 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             let channelModel = self.arrM[indexPath.row] as! channelModel
             cell.textLabel?.text = channelModel.name
             cell.backgroundColor = UIColor.brown
-            
         } else {
             let channelModel  = self.arrM[indexPath.row] as! Subject;
             cell.textLabel?.text = channelModel.name
             cell.backgroundColor = hexColor(hex: 0xffbb33)
         }
-       
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         let channelModel  = self.arrM[indexPath.row] as! Subject;
-         let vc = ChannelViewController()
-        vc.title = channelModel.name
-        vc.channleID = channelModel.channel_id
+        let vc = ChannelViewController()
+        if self.isAfn {
+            let  channelModel = self.arrM[indexPath.row] as! channelModel
+            vc.title = channelModel.name
+            vc.channleID = channelModel.channel_id
+            
+        } else {
+            let channelModel  = self.arrM[indexPath.row] as! Subject;
+            vc.title = channelModel.name
+            vc.channleID = channelModel.channel_id
+        }
         self.navigationController?.pushViewController(vc, animated: true)
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "AFN请求", style: .done, target: self, action: #selector(afnNetWork))
         self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "MOYA请求", style: .done, target: self, action: #selector(moyaNet))
-        
         self.arrM = NSMutableArray(array: []) //初始化可变数组
         self.isAfn = false
         self.initTableView()
--- motaDemo/networkManager.swift
+++ motaDemo/networkManager.swift
@@ -11,7 +11,15 @@ import PPNetworkHelper
 import MBProgressHUD
 //单例管理AFN的网络请求,闭包的,使用尾随闭包需要使用@escaping修饰,闭包默认是非逃逸的,修饰之后,变成逃逸的,通俗点说就是这个闭包在函数执行完成之后才被调用,但是逃逸闭包要考虑闭包内的self的持有,此时与block类似,非逃逸闭包是不用考虑self的持有,是较为安全的
 class networkManager {
-    static let `default` = networkManager() //swift的单例
+    static let `default` = networkManager() //swift的单例,就这么一句话
+    
+    /// 网络请求
+    ///
+    /// - Parameters:
+    ///   - url: 请求的地址
+    ///   - parm: 请求的参数
+    ///   - success: 成功的回调
+    ///   - failure: 失败的回调
     func request(url:String,parm:[String:Any],success: @escaping (Any?) -> (),failure: @escaping (Error?) -> ()) {
         let url = HOSTAPI.appending(url)
         MBProgressHUD.showAdded(to: UIApplication.shared.keyWindow!, animated: true)
